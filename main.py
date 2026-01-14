74fa34be98f7ab56aefaf0973d2343559d618060
Yarden Aruch <yardenaruch@mail.tau.ac.il>
2026-01-14 20:06:02 +0200

    update index, login, register html pages and css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from utils import db_cursor
 from datetime import datetime
 
