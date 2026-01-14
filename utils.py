0812dd21b7e2581dec14b758ca7c653b863fa6bc
Yarden Aruch <yardenaruch@mail.tau.ac.il>
2026-01-14 18:31:57 +0000

    Resolve merge conflicts using GitHub version

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
+import mysql.connector
 from contextlib import contextmanager
 
 @contextmanager
 def db_cursor():
-    raise RuntimeError("DB credentials are configured on PythonAnywhere only.")
-    yield
+    connection = mysql.connector.connect(
+        host="yardenaruch.mysql.pythonanywhere-services.com",
+        user="yardenaruch",
+        password="ily3hap3",
+        database="yardenaruch$FLYTAUdb",
+        autocommit=True
+    )
+    try:
+        cursor = connection.cursor(dictionary=True)
+        yield cursor
+    finally:
+        cursor.close()
+        connection.close()
